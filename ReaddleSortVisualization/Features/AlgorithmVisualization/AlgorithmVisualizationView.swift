081a7990cce7e994a76aad6793a4cec12bc6f19f
Yulian Baranetskyy <yulian.baranetskyy@gmail.com>
2024-02-06 17:08:15 +0200

    Implemented stepping through algorithm

--- ReaddleSortVisualization/Features/AlgorithmVisualization/AlgorithmVisualizationView.swift
+++ ReaddleSortVisualization/Features/AlgorithmVisualization/AlgorithmVisualizationView.swift
@@ -11,42 +11,62 @@ struct AlgorithmVisualizationView: View {
     @ObservedObject var viewModel: AlgorithmVisualizationViewModel
     
     var body: some View {
-            VStack {
-                TextField("Enter numbers separated by commas", text: Binding<String>(
-                    get: { self.viewModel.inputArray.map { String($0) }.joined(separator: ", ") },
-                    set: {
-                        self.viewModel.inputArray = $0.split(separator: ",").compactMap { Int($0.trimmingCharacters(in: .whitespaces)) }
-                    }
-                ))
-                .keyboardType(.decimalPad)
-                .padding()
-                
-                Button(action: {
-                    UIApplication.shared.endEditing()
-                    self.viewModel.sortArray()
-                }) {
-                    Text(viewModel.isSorting ? "Sorting..." : "Sort")
+        VStack {
+            TextField("Enter numbers separated by commas", text: Binding<String>(
+                get: { self.viewModel.inputArray.map { String($0) }.joined(separator: ", ") },
+                set: {
+                    self.viewModel.inputArray = $0.split(separator: ",").compactMap { Int($0.trimmingCharacters(in: .whitespaces)) }
                 }
-                .buttonStyle(.bordered)
-                .disabled(viewModel.isTextFieldEmpty || viewModel.isSorting)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 10) {
-                        ForEach(viewModel.sortedArray, id: \.self) { number in
-                            Text("\(number)")
-                                .frame(width: 30, height: 30)
-                                .background(Color.blue)
-                                .cornerRadius(15)
-                                .foregroundColor(.white)
-                                .animation(.default, value: UUID())
-                        }
-                    }
-                }
-                .padding()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            ))
+            .keyboardType(.decimalPad)
             .padding()
+            
+            Button(action: {
+                UIApplication.shared.endEditing()
+                self.viewModel.sortArray()
+            }) {
+                Text(viewModel.isSorting ? "Sorting..." : "Sort")
+            }
+            .buttonStyle(.bordered)
+            .disabled(viewModel.isTextFieldEmpty || viewModel.isSorting)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 10) {
+                    ForEach(viewModel.currentOutputArray, id: \.self) { number in
+                        Text("\(number)")
+                            .frame(width: 30, height: 30)
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                            .foregroundColor(.white)
+                            .animation(.default, value: UUID())
+                    }
+                }
+            }
+            .padding()
+            
+            if !viewModel.isSorting {
+                HStack {
+                    Button(action: {
+                        self.viewModel.stepBackward()
+                    }) {
+                        Image(systemName: "arrow.left")
+                    }
+                    .disabled(viewModel.currentStepIndex == 0)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        self.viewModel.stepForward()
+                    }) {
+                        Image(systemName: "arrow.right")
+                    }
+                    .disabled(viewModel.currentStepIndex == viewModel.stepArrays.count - 1 || viewModel.currentOutputArray.isEmpty)
+                }
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .padding()
+    }
 }
 
 //#Preview {
--- ReaddleSortVisualization/Features/AlgorithmVisualization/AlgorithmVisualizationViewModel.swift
+++ ReaddleSortVisualization/Features/AlgorithmVisualization/AlgorithmVisualizationViewModel.swift
@@ -14,9 +14,12 @@ class AlgorithmVisualizationViewModel: ObservableObject {
     private let sortingAlgorithm: SortingAlgorithm
     
     @Published var inputArray: [Int] = []
-    @Published var sortedArray: [Int] = []
+    @Published var stepArrays: [[Int]] = []
+    @Published var currentOutputArray: [Int] = []
     
     @Published var isSorting: Bool = false
+    
+    @Published var currentStepIndex: Int = 0
     
     var isTextFieldEmpty: Bool {
         return inputArray.isEmpty
@@ -27,6 +30,8 @@ class AlgorithmVisualizationViewModel: ObservableObject {
     }
     
     func sortArray() {
+        currentStepIndex = 0
+        
         self.isSorting = true
         
         sortingAlgorithm.sort(inputArray, stepDelaySeconds: 0.5)
@@ -35,8 +40,29 @@ class AlgorithmVisualizationViewModel: ObservableObject {
             .sink(receiveCompletion: { _ in
                 self.isSorting = false
             }, receiveValue: { [weak self] sortedArray in
-                self?.sortedArray = sortedArray
+                self?.stepArrays.append(sortedArray)
+                self?.currentOutputArray = sortedArray
+                self?.currentStepIndex += 1
             })
             .store(in: &cancellables)
+    }
+    
+    func stepForward() {
+        guard currentStepIndex < stepArrays.count - 1 else { return }
+        currentStepIndex += 1
+        
+        if stepArrays.count > currentStepIndex {
+            currentOutputArray = stepArrays[currentStepIndex]
+        }
+    }
+    
+    func stepBackward() {
+        guard currentStepIndex > 0 else { return }
+        
+        currentStepIndex -= 1
+        
+        if stepArrays.count > currentStepIndex {
+            currentOutputArray = stepArrays[currentStepIndex]
+        }
     }
 }
